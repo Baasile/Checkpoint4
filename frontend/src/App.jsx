daad163f28b05b09959bd165f113b9435f10a965
Baasile <mathieu.basile@gmail.com>
2022-07-19 18:51:01 +0200

    install tailwind, axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import Home from "@pages/Home";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Home />
-      <p>coucou</p>
+      <p className="text-blue-400">coucou</p>
+      <h2 className="text-4xl text-red-400">waaaaa</h2>
     </div>
   );
 }
